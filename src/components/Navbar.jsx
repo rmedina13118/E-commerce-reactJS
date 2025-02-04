5809a9697c857ffc493bacd93af06c5c06bda5e5
Richmedi13 <echemedirichm@gmail.com>
2025-02-04 05:21:50 -0500

    launch project

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,23 @@
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink } from 'react-router-dom'
+import Logo from '../assets/logo.png'
 
-function Navbar() {
+function Navbar () {
   return (
-    <nav className='navbar'>
-      <ul>
+    <nav className='flex justify-between px-20 items-center max-w-[100vw] bg-[#ffb700] text-black font-bold text-2xl '>
+      <img src={Logo} width={100} height={100} className='w-20' />
+      <ul className='flex flex-row gap-8'>
         <li>
-          <NavLink to={"/"}>Home</NavLink>
+          <NavLink to={'/'}>Home</NavLink>
         </li>
         <li>
-          <NavLink to={"/categories"}>Productos</NavLink>
+          <NavLink to={'/categorias'}>Productos</NavLink>
         </li>
         <li>
-          <NavLink to={"/"}>Contacto</NavLink>
+          <NavLink to={'/contacto'}>Contacto</NavLink>
         </li>
       </ul>
     </nav>
-  );
+  )
 }
 
-export default Navbar;
+export default Navbar
