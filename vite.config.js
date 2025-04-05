686fda9ac4f8beefb31f4f7af6cab9f9b46c8fe9
Richard Medina Echeverri <RMEDINA@NUTRABIOTICS.LOCAL>
2025-04-05 02:17:06 -0500

    debugging

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'node:path';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    react()],
+  plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, '/src'),
+      '@': path.resolve(__dirname, 'src'),
     },
-    server: {
-      port: 3000,
-      open: true,
-    }
   },
-})
+});
